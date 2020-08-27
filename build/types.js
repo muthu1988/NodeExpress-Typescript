622ed7ec9ffe36ea0bebe28112ae9537db759ce3
Muthu Kumaran A <muthukumarana@outlook.com>
2020-08-27 15:36:22 +0530

    Removed buid file

--- build/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TYPES = void 0;
-var TYPES = {
-    Controller: Symbol.for("Controller"),
-    Service: Symbol.for("Service"),
-    Provider: Symbol.for("Provider")
-};
-exports.TYPES = TYPES;
